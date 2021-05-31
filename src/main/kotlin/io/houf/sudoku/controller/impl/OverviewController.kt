5f7e46962a4f353c5b7b1b18e8e7ef459834fcb0
Rick van den Heuvel <Rick.heuvel@outlook.com>
2021-05-31 15:16:42 +0200

    added puzzles to OverviewController

--- src/main/kotlin/io/houf/sudoku/controller/impl/OverviewController.kt
+++ src/main/kotlin/io/houf/sudoku/controller/impl/OverviewController.kt
@@ -2,10 +2,19 @@ package io.houf.sudoku.controller.impl
 
 import io.houf.sudoku.Sudoku
 import io.houf.sudoku.controller.Controller
+import io.houf.sudoku.util.PuzzleReader
 import io.houf.sudoku.view.View
 import io.houf.sudoku.view.impl.OverviewView
+import io.houf.sudoku.widget.impl.ButtonWidget
 
 class OverviewController(sudoku: Sudoku) : Controller<OverviewController>(sudoku) {
+
+    var puzzles = emptyList<Triple<String, String, String>>()
+
+    init {
+        puzzles = PuzzleReader.read()
+    }
+
     fun pushGame() {
         sudoku.push(GameController::class.java)
     }
