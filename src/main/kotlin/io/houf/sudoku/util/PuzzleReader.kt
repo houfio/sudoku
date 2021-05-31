cc2659d9b26ba593194266118aaea204f2434ac6
Rick van den Heuvel <Rick.heuvel@outlook.com>
2021-05-31 16:02:05 +0200

    removed hardcoded files from puzzlereaders

--- src/main/kotlin/io/houf/sudoku/util/PuzzleReader.kt
+++ src/main/kotlin/io/houf/sudoku/util/PuzzleReader.kt
@@ -1,11 +1,35 @@
 package io.houf.sudoku.util
 
+import java.io.IOException
+import java.net.URI
+import java.net.URISyntaxException
+import java.nio.file.*
+import kotlin.io.path.extension
+import kotlin.io.path.isDirectory
+import kotlin.io.path.nameWithoutExtension
+import kotlin.io.path.readText
+
+
 object PuzzleReader {
-    private val paths = listOf("puzzle.4x4", "puzzle.6x6", "puzzle.9x9", "puzzle.jigsaw", "puzzle.samurai")
-
-    fun readPuzzles() = paths.map {
-        Triple(it.substringBeforeLast('.'), it.substringAfterLast('.'), readContents(it))
+    
+    fun readPuzzles(): List<Triple<String, String, String?>> {
+        val uri: URI = this::class.java.getResource("/puzzles").toURI()
+        val myPath: Path
+        val list = mutableListOf<Triple<String,String,String?>>()
+        if (uri.getScheme().equals("jar")) {
+            val fileSystem: FileSystem = FileSystems.newFileSystem(uri, emptyMap<String, Any>())
+            myPath = fileSystem.getPath("/resources")
+        } else {
+            myPath = Paths.get(uri)
+        }
+        val walk = Files.walk(myPath, 1)
+        val it = walk.iterator()
+        while (it.hasNext()) {
+            val file = it.next()
+            if (!file.isDirectory())
+                list.add(
+                    Triple(file.nameWithoutExtension, file.extension, file.readText()))
+        }
+        return list
     }
-
-    private fun readContents(file: String) = this::class.java.getResource("/puzzles/$file")?.readText(Charsets.UTF_8)
 }
