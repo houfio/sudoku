41153607a5614ff2df90feb95a9e0d57c786df01
houfio <26674115+houfio@users.noreply.github.com>
2021-05-31 13:02:33 +0200

    Update docs

--- src/main/kotlin/io/houf/sudoku/view/View.kt
+++ src/main/kotlin/io/houf/sudoku/view/View.kt
@@ -5,7 +5,7 @@ import io.houf.sudoku.widget.Widget
 import io.houf.sudoku.widget.WidgetGroup
 import io.houf.sudoku.widget.impl.FrameWidgetGroup
 
-abstract class View<T : Controller<T>>(controller: T, vararg children: Widget) : WidgetGroup(
+abstract class View<T : Controller<T>>(private val controller: T, vararg children: Widget) : WidgetGroup(
     *children,
     FrameWidgetGroup(controller)
 )
