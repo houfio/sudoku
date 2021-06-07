8d65a23581c19a7b77cda5ab76b38ec3adeff354
Rick van den Heuvel <Rick.heuvel@outlook.com>
2021-06-07 17:16:18 +0200

    kei mooi spul

--- src/main/kotlin/io/houf/sudoku/controller/impl/GameController.kt
+++ src/main/kotlin/io/houf/sudoku/controller/impl/GameController.kt
@@ -10,14 +10,12 @@ import io.houf.sudoku.view.View
 import io.houf.sudoku.view.impl.GameView
 
 class GameController(sudoku: Sudoku) : Controller<GameController>(sudoku) {
-    private var state: State? = null
+
 
     var errors: List<Pair<Int, Int>> = listOf()
         private set
 
-    init {
-        state = PlayState()
-    }
+
 
     fun getSize() = sudoku.game.puzzle?.size ?: 1
 
@@ -26,16 +24,11 @@ class GameController(sudoku: Sudoku) : Controller<GameController>(sudoku) {
     fun getTiles() = sudoku.game.puzzle?.getTiles() ?: emptyList()
 
     fun switchMode(){
-        state = if (state is PlayState){
-            EditorState()
-        }else{
-            PlayState()
-        }
-        println(state)
+        sudoku.game.switchMode()
     }
     fun enter(x: Int, y: Int, char: Char?) {
         errors = listOf()
-        state?.enter(sudoku, x, y, char)
+        sudoku.game.state.enter(sudoku, x, y, char)
     }
 
     fun validate() {
--- src/main/kotlin/io/houf/sudoku/model/Game.kt
+++ src/main/kotlin/io/houf/sudoku/model/Game.kt
@@ -4,12 +4,19 @@ import io.houf.sudoku.model.command.Command
 import io.houf.sudoku.model.command.CommandExecutor
 import io.houf.sudoku.model.puzzle.Puzzle
 import io.houf.sudoku.model.puzzle.PuzzleCandidate
+import io.houf.sudoku.util.state.EditorState
+import io.houf.sudoku.util.state.PlayState
+import io.houf.sudoku.util.state.State
 
 class Game {
     private val executor = CommandExecutor(this)
 
+    var state: State = PlayState()
+        private set
     var puzzleCandidate: PuzzleCandidate? = null
     var puzzle: Puzzle? = null
+
+
 
     fun execute(command: Command<Game>) {
         executor.execute(command)
@@ -17,5 +24,14 @@ class Game {
 
     fun rollback() {
         executor.rollback()
+    }
+
+    fun switchMode() {
+        state = if (state is PlayState){
+            EditorState()
+        }else{
+            PlayState()
+        }
+        println(state)
     }
 }
