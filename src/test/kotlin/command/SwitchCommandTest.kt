5a9db8ad87e956241eb3f227d71fd937f101cc34
Rick van den Heuvel <Rick.heuvel@outlook.com>
2021-06-09 14:08:27 +0200

    Command tests done
    added switchCommandTests

---
+++ src/test/kotlin/command/SwitchCommandTest.kt
@@ -0,0 +1,41 @@
+package command
+
+import io.houf.sudoku.model.GameData
+import io.houf.sudoku.model.command.impl.SwitchCommand
+import io.houf.sudoku.model.state.impl.NoteState
+import io.houf.sudoku.model.state.impl.PlayState
+import org.junit.jupiter.api.Test
+import org.koin.core.component.getScopeName
+import kotlin.test.assertEquals
+
+internal class SwitchCommandTest {
+
+    fun arrange(): Pair<GameData, SwitchCommand> {
+
+        val data = GameData()
+        data.state = PlayState()
+
+        val context = SwitchCommand()
+
+        return data to context
+    }
+
+    @Test
+    fun testExecute() {
+        val (data, command) = arrange()
+
+        command.execute(data)
+
+        assertEquals(data.state.getScopeName(), NoteState().getScopeName())
+    }
+
+    @Test
+    fun testRollback() {
+        val (data, command) = arrange()
+
+        command.execute(data)
+        command.rollback(data)
+
+        assertEquals(data.state.getScopeName(), PlayState().getScopeName())
+    }
+}
