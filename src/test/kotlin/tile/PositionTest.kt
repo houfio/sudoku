3d6f1e7213b857d5a42dfa3222c7a7724fa188e1
Rick van den Heuvel <Rick.heuvel@outlook.com>
2021-06-10 14:15:28 +0200

    positionTest done

---
+++ src/test/kotlin/tile/PositionTest.kt
@@ -0,0 +1,52 @@
+package tile
+
+import io.houf.sudoku.model.tile.Position
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class PositionTest {
+
+    fun arrange(): Position {
+        val position = Position(5,5)
+        return position
+    }
+    @Test
+    fun topTest(){
+        val position = arrange()
+
+        val value: Position = position.top()
+
+        assertTrue { value == Position(5,4) }
+    }
+
+    @Test
+    fun bottomTest(){
+        val position = arrange()
+
+        val value: Position = position.bottom()
+
+        println(value)
+        assertTrue { value == Position(5,6) }
+    }
+
+    @Test
+    fun leftTest(){
+        val position = arrange()
+
+        val value: Position = position.left()
+
+        println(value)
+        assertTrue { value == Position(4,5) }
+    }
+
+    @Test
+    fun rightTest(){
+        val position = arrange()
+
+        val value: Position = position.right()
+
+        println(value)
+        assertTrue { value == Position(6,5) }
+    }
+}
